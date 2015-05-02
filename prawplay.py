cd2b917fe244bc68ca2eff90c0b8176ed78c5665
Alexandra Murray <horisustar@gmail.com>
2015-05-01 23:44:34 -0400

    Added ability to customize post text and accept urls in body

--- prawplay.py
+++ prawplay.py
@@ -1,10 +1,23 @@
+import inspect
+import re
 import praw
 
+def getURL(contentBody):
+    try:
+        return re.search("(?P<url>https?://[^\s]+)", contentBody).group("url")
+    except AttributeError:
+        return "no URL found"
+    
+def getTitle(contentBody):
+    return c.split("\n")[3]
+
 sub = 'subreddit'
-r = praw.Reddit(user_agent='posts unread messages to empoknor')
+r = praw.Reddit(user_agent='posts unread messages to a subreddit')
 
 r.login('username', 'password')
 for content in r.get_unread():
-    print content
+    url = getURL(content.body)
+    title = getTitle(content.body)
     r.submit(sub, title, text='texty', url=None, captcha=None, save=None, send_replies=None, resubmit=None)
-    r.mark_as_read() #does this work?
+    #r.mark_as_read()
+    
