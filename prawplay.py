1440f22d649388ac7d753b0c1057b5e54f30b070
Alexandra Murray <horisustar@gmail.com>
2015-05-02 15:32:25 -0400

    Added ability to mark messages as read

--- prawplay.py
+++ prawplay.py
@@ -1,6 +1,6 @@
-import inspect
 import re
 import praw
+
 
 def getURL(contentBody):
     try:
@@ -9,15 +9,16 @@ def getURL(contentBody):
         return "no URL found"
     
 def getTitle(contentBody):
-    return c.split("\n")[3]
+    return contentBody.split("\n")[3]
 
-sub = 'subreddit'
-r = praw.Reddit(user_agent='posts unread messages to a subreddit')
 
-r.login('username', 'password')
+SUB = 'EmpokNor'
+r = praw.Reddit(user_agent='posts unread messages to SUB')
+
+r.login('user', 'password')
 for content in r.get_unread():
+    print content.body
     url = getURL(content.body)
     title = getTitle(content.body)
-    r.submit(sub, title, text='texty', url=None, captcha=None, save=None, send_replies=None, resubmit=None)
-    #r.mark_as_read()
-    
+    r.submit(SUB, title, text=None, url=url, captcha=None, save=None, send_replies=None, resubmit=None)
+    content.mark_as_read()
